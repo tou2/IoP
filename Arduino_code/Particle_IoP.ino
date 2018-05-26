ca6464f5f153967ec876f8641fb1c9fe741abaee
tou2 <tarek.alsabagh@gmail.com>
2018-05-26 15:11:07 -0400

    agtechx

--- Arduino_code/Particle_IoP.ino
+++ Arduino_code/Particle_IoP.ino
@@ -1,33 +1,40 @@
+// This #include statement was automatically added by the Particle IDE.
+#include <Adafruit_DHT.h>
+
 
 #include <ThingSpeak.h>
 
-#include "Adafruit_DHT_Particle.h"
 
-#define DHTPIN D2     // what pin we're connected to
+//#define DHTPIN D2     // what pin we're connected to
 
-#define DHTTYPE DHT22		// DHT 22 (AM2302)
+//#define DHTTYPE DHT22		// DHT 22 (AM2302)
+#include "ThingSpeak.h"
 
 TCPClient client;
 
-unsigned long myChannelNumber = ******;		/*Thingspeak channel id*/
-const char * myWriteAPIKey = "****************";/*Channel's write API key*/
+#include "Adafruit_DHT.h"
+
+unsigned long myChannelNumber = 443542;		/*Thingspeak channel id*/
+const char * myWriteAPIKey = "4V4UQ5VL59D22LNR";/*Channel's write API key*/
 
 
-int photoCell = A0;
+int photoCell = A3;
 int power = A5; // Photocell power.  An analog pin to gives a more steady voltage.
 int light; // Light
 
-int Moisture = A1;
+int Moisture = A0;
 //int power = A5; // Photocell power.  An analog pin to gives a more steady voltage.
 int moist; // Light
 
-DHT dht(DHTPIN, DHTTYPE);
+DHT dht(D2, DHT22);
+
+//DHT dht(DHTPIN, DHTTYPE);
 int loopCount;
 
 void setup() {
-	Serial.begin(9600); 
-	Serial.println("DHTxx test!");
-	Particle.publish("state", "DHTxx test start");
+//	Serial.begin(9600); 
+//	Serial.println("DHTxx test!");
+	//Particle.publish("state", "DHTxx test start");
     ThingSpeak.begin(client);
 	dht.begin();
 	
@@ -57,72 +64,76 @@ void loop() {
 // very slow sensor)
 	float h = dht.getHumidity();
 // Read temperature as Celsius
-	float t = dht.getTempCelcius();
+//	float t = dht.getTempCelcius();
 // Read temperature as Farenheit
 	float f = dht.getTempFarenheit();
   
 // Check if any reads failed and exit early (to try again).
-	if (isnan(h) || isnan(t) || isnan(f)) {
+ if (isnan(h) || isnan(f)) {
 		Serial.println("Failed to read from DHT sensor!");
 		return;
 	}
 
 // Compute heat index
 // Must send in temp in Fahrenheit!
-	float hi = dht.getHeatIndex();
+/*	float hi = dht.getHeatIndex();
 	float dp = dht.getDewPoint();
 	float k = dht.getTempKelvin();
+	*/
 
  ThingSpeak.setField(1,h);
-  ThingSpeak.setField(2,t);
+  ThingSpeak.setField(2,f);
    ThingSpeak.setField(3,light);
   ThingSpeak.setField(4,moist);
 
   // Write the fields that you've set all at once.
   ThingSpeak.writeFields(myChannelNumber, myWriteAPIKey);  
-  delay(1000);	
+  delay(20000);	
+ 
 
-// ThingSpeak.writeField(myChannelNumber, 1, h, myWriteAPIKey);
-   // delay(1500); /* ThingSpeak will only accept updates every 15 seconds*/
-  
-//    ThingSpeak.writeField(myChannelNumber, 2, t, myWriteAPIKey);
-   // delay(1500); /* ThingSpeak will only accept updates every 15 seconds.*/
-      
-//    ThingSpeak.writeField(myChannelNumber, 3, light, myWriteAPIKey);
-  //  delay(1500); /* ThingSpeak will only accept updates every 15 seconds.*/
+//ThingSpeak.writeField(myChannelNumber, 1, light, myWriteAPIKey);
+ //delay(1500); /* ThingSpeak will only accept updates every 15 seconds.*/
+ 
+ //ThingSpeak.writeField(myChannelNumber, 2, moist, myWriteAPIKey);
+// delay(1500); /* ThingSpeak will only accept updates every 15 seconds.*/
+ 
+// ThingSpeak.writeField(myChannelNumber, 3, h, myWriteAPIKey);
+ //delay(1500); /* ThingSpeak will only accept updates every 15 seconds*/
+ //
+// ThingSpeak.writeField(myChannelNumber, 4, f, myWriteAPIKey);
+ //delay(1500); /* ThingSpeak will only accept updates every 15 seconds.*/
+ 
 
-   // ThingSpeak.writeField(myChannelNumber, 4, moist, myWriteAPIKey);
-//  //  delay(1500); /* ThingSpeak will only accept updates every 15 seconds.*/
   
-  /* 
+  /*
     Serial.print("light"); 
 	Serial.print(light);
 	Serial.print("Humid: "); 
 	Serial.print(h);
 	Serial.print("% - ");
-	Serial.print("Temp: "); 
-	Serial.print(t);
-	Serial.print("*C ");
+//	Serial.print("Temp: "); 
+//	Serial.print(t);
+//	Serial.print("*C ");
 	Serial.print(f);
 	Serial.print("*F ");
-	Serial.print(k);
-	Serial.print("*K - ");
-	Serial.print("DewP: ");
-	Serial.print(dp);
-	Serial.print("*C - ");
-	Serial.print("HeatI: ");
-	Serial.print(hi);
-	Serial.println("*C");
-	Serial.println(Time.timeStr());
+//	Serial.print(k);
+//	Serial.print("*K - ");
+//	Serial.print("DewP: ");
+//	Serial.print(dp);
+//	Serial.print("*C - ");
+//	Serial.print("HeatI: ");
+//	Serial.print(hi);
+//	Serial.println("*C");
+//	Serial.println(Time.timeStr());
 	*/
-	//String timeStamp = Time.timeStr();
-//	Particle.publish("readings", String::format("{\light: light, \"Hum(\%)\": %4.2f, \"Temp(°C)\": %4.2f, \"DP(°C)\": %4.2f, \"HI(°C)\": %4.2f}",light, h, t, dp, hi));
-//	delay(1500);
+//	String timeStamp = Time.timeStr();
+//	Particle.publish("readings", String::format("{\light: light, \"Hum(\%)\": %4.2f}",light, h, f));
+delay(1500);
 	loopCount++;
-	if(loopCount >= 1){
+	if(loopCount >= 5){
 	  Particle.publish("state", "Going to sleep for 1/2 minutes");
-	  delay(1000);
-	  System.sleep(SLEEP_MODE_DEEP, 3600);  
+	  delay(10000);
+	  System.sleep(SLEEP_MODE_DEEP, 5);  
 	}
 }
 
